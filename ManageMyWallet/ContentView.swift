172a98e699e22c451baf926eb4c765b0666abcd6
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2022-07-28 08:54:59 +0100

    Initial Commit

---
+++ ManageMyWallet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ManageMyWallet
+//
+//  Created by Bouchedoub Rmazi on 28/7/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ManageMyWallet/ManageMyWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManageMyWalletApp.swift
+//  ManageMyWallet
+//
+//  Created by Bouchedoub Rmazi on 28/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct ManageMyWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
